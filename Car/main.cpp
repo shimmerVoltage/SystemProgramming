e126fe57a16bb0494fe0ea66746d547aefb8aa87
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-04-24 21:52:58 +0300

    Panel & Fill DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -1,7 +1,10 @@
 ﻿#include<Windows.h>
 #include<iostream>
 #include<conio.h>
-using namespace std;
+#include<thread>
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define Enter		13
 #define Escape		27
@@ -122,6 +125,10 @@ class Car
 	int speed;
 	const int MAX_SPEED;
 	bool driver_inside;
+	struct
+	{
+		std::thread panel_thread;
+	}threads_container;
 public:
 	Car(double consumption, int capacity, int max_speed = 250):
 		MAX_SPEED
@@ -144,11 +151,15 @@ public:
 	void get_in()
 	{
 		driver_inside = true;
-		panel();
+		threads_container.panel_thread = std::thread(&Car::panel, this);
+		//panel();
 	}
 	void get_out()
 	{
 		driver_inside = false;
+		if (threads_container.panel_thread.joinable())threads_container.panel_thread.join();
+		system("CLS");
+		cout << "You are out of the Car" << endl;
 	}
 	void control()
 	{
@@ -161,6 +172,13 @@ public:
 			case Enter:
 				driver_inside ? get_out() : get_in();
 				break;
+			case 'F':case'f':
+				double fuel;
+				cout << "Введите объем топлива: "; cin >> fuel;
+				tank.fill(fuel);
+				break;
+			case Escape:
+				get_out();
 			}
 		} while (key != Escape);
 	}
@@ -189,6 +207,7 @@ public:
 
 void main()
 {
+	setlocale(LC_ALL, "");
 #ifdef TANK_CHECK
 	setlocale(LC_ALL, "");
 	Tank tank(80);
